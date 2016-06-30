e1c26019eb83413c017d0680a0efe0b6b182ef77
lawvs <lawvs@users.noreply.github.com>
2016-06-30 21:42:51 +0800

    大体完成Game，该类现在能够正常运行
    优化完成方法slideTo方法。
    增加计算分数并保存最高分功能，但是因为undo堆栈没有保存分数，因此undo之后当前分数不会恢复为旧分数，使用undo之后分数减半的方法暂时修补无法回复的缺陷。
    添加变量步数step用于记录游戏步数。
    现在已经可以在GameTest类中愉快地使用命令行玩游戏了。
    类中还没有做动画接口。

--- src/pers/jmu/model/Game.java
+++ src/pers/jmu/model/Game.java
@@ -18,6 +18,11 @@ import java.util.Stack;
  * @version 创建时间：2016年6月26日16:30:00
  */
 public class Game {
+	// 动作状态变量
+	public final static int SLIDE_UP = 1;
+	public final static int SLIDE_DOWN = 2;
+	public final static int SLIDE_LEFT = 3;
+	public final static int SLIDE_RIGHT = 4;
 
 	private int height;// 列数
 	private int width;// 行数
@@ -25,6 +30,11 @@ public class Game {
 	private List<Point> emptyCard;// 保存空卡片坐标
 	private Stack<Card[][]> undoStack;// 储存游戏旧状态 在卡片状态改变前存储
 	private int maxUndo;// 最大可undo次数
+	private int maxScore;
+
+	private int nowScore;
+	private boolean isChange;// 该次移动行为是否有效
+	private int step;// 行走步数
 
 	/**
 	 * 初始化游戏
@@ -71,6 +81,8 @@ public class Game {
 	 * 初始化 重新开始游戏
 	 */
 	public void restart() {
+		nowScore = 0;
+		step = 0;
 		emptyCard.clear();
 		undoStack.clear();
 		// 初始化卡片
@@ -151,16 +163,168 @@ public class Game {
 	}
 
 	/**
+	 * 将game向一个方向移动 上下左右
+	 * 
+	 * @param direction
+	 *            移动方向 SLIDE_UP SLIDE_DOWN ...
+	 * @return 移动是否成功
+	 * @version 创建时间：2016年6月30日20:54:44
+	 */
+	public boolean slideTo(int direction) {
+		isChange = false;// 此次移动是否有效
+		saveStatus();// 可能引发小错误 undo次数可能减少
+
+		switch (direction) {
+		case SLIDE_UP:
+			slideUP();
+			break;
+		case SLIDE_DOWN:
+			slideDown();
+			break;
+		case SLIDE_LEFT:
+			slideLeft();
+			break;
+		case SLIDE_RIGHT:
+			slideRight();
+			break;
+		default:
+			return false;
+		}
+
+		// 重新计算空卡片位置
+		calcEmptyCard();
+		if (emptyCard.isEmpty()) {
+			// 游戏结束
+			gameover();
+			return false;
+		}
+		if (isChange) {// 此次移动有效
+			step++;//移动有效，步数加1
+			addNewCard();
+		} else {// 移动无效s
+			undoStack.pop();// 删除保存的状态 可能引起undo次数减少
+		}
+		return true;
+	}
+
+	/**
+	 * 将game向上滑动
+	 * 
+	 * @return 移动是否成功
+	 * @version 创建时间：2016年6月30日20:50:00
+	 */
+	private boolean slideUP() {
+		// 移动卡片 slideUP
+		for (int y = 0; y < cardStatus[0].length; y++) {
+			int tx = 0;// 临时x
+			int ty = 0;// 临时y
+			int v = -1;// 保留前一个方格的值
+			for (int x = 0; x < cardStatus.length; x++) {
+				// 值为0
+				if (cardStatus[x][y].getValue() == 0) {
+					// 尝试找到非0值填充，或进行合并
+					for (int i = x + 1; i < cardStatus.length; i++) {
+						// 找到值和空格前格子值相同，进行合并
+						if (cardStatus[i][y].getValue() == v) {
+							// 合并操作
+							merge(tx, ty, i, y);
+							x--;// 合并后格子变为0，需重新检查
+							// 清空临时变量
+							v = -1;// 不允许重复合并，不允许如2 2 2 2直接变为4
+							tx = 0;
+							ty = 0;
+							break;
+						} else if (cardStatus[i][y].getValue() != 0) {
+							v = cardStatus[i][y].getValue();
+							tx = x;
+							ty = y;
+							move(x, y, i, y);
+							break;
+						}
+					} // end of for
+
+				} else if (cardStatus[x][y].getValue() == v) {
+					// 合并操作
+					merge(tx, ty, x, y);
+					x--;// 合并后格子变为0，需重新检查
+					v = -1;
+					tx = 0;
+					ty = 0;
+
+				} else {
+					v = cardStatus[x][y].getValue();
+					tx = x;
+					ty = y;
+				} // end of if
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * 将game向下滑动
+	 * 
+	 * @return 移动是否成功
+	 * @version 创建时间：2016年6月29日23:27:00
+	 */
+	private boolean slideDown() {
+		// 移动卡片 slideDown
+		for (int y = 0; y < cardStatus[0].length; y++) {
+			int tx = 0;// 临时x
+			int ty = 0;// 临时y
+			int v = -1;// 保留前一个方格的值
+			for (int x = cardStatus.length - 1; x >= 0; x--) {
+				// 值为0
+				if (cardStatus[x][y].getValue() == 0) {
+					// 尝试找到非0值填充，或进行合并
+					for (int i = x - 1; i >= 0; i--) {
+						// 找到值和空格前格子值相同，进行合并
+						if (cardStatus[i][y].getValue() == v) {
+							// 合并操作
+							merge(tx, ty, i, y);
+							x++;// 合并后格子变为0，需重新检查
+							// 清空临时变量
+							v = -1;// 不允许重复合并，不允许如2 2 2 2直接变为4
+							tx = 0;
+							ty = 0;
+							break;
+						} else if (cardStatus[i][y].getValue() != 0) {
+							v = cardStatus[i][y].getValue();
+							tx = x;
+							ty = y;
+							move(x, y, i, y);
+							break;
+						}
+					} // end of for
+
+				} else if (cardStatus[x][y].getValue() == v) {
+					// 合并操作
+					merge(tx, ty, x, y);
+					x++;// 合并后格子变为0，需重新检查
+					v = -1;
+					tx = 0;
+					ty = 0;
+
+				} else {
+					v = cardStatus[x][y].getValue();
+					tx = x;
+					ty = y;
+				} // end of if
+			}
+		}
+
+		return true;
+	}
+
+	/**
 	 * 将game向左滑动
 	 *
 	 * @return 移动是否成功
 	 * @version 创建时间：2016年6月28日22:00:00
 	 */
-	public boolean slideLeft() {
-		boolean isChange = false;// 此次移动是否有效
-		saveStatus();// 可能引发小错误
-		// 移动卡片
-		// slideLeft
+	private boolean slideLeft() {
+		// 移动卡片 slideLeft
 		for (int x = 0; x < cardStatus.length; x++) {
 			int tx = 0;// 临时x
 			int ty = 0;// 临时y
@@ -168,38 +332,30 @@ public class Game {
 			for (int y = 0; y < cardStatus[x].length; y++) {
 				// 值为0
 				if (cardStatus[x][y].getValue() == 0) {
-					// 找到非0值填充
+					// 尝试找到非0值填充，或进行合并
 					for (int i = y + 1; i < cardStatus[x].length; i++) {
+						// 找到值和空格前格子值相同，进行合并
 						if (cardStatus[x][i].getValue() == v) {
 							// 合并操作
-							cardStatus[tx][ty].setValue(2 * v);
-							// System.out.println(x+" " +y+ " "+2*v);
-							cardStatus[x][i].setValue(0);
-							isChange = true;// 格子合并
-							v = -1;
+							merge(tx, ty, x, i);
+							// 清空临时变量
+							v = -1;// 不允许重复合并，不允许如2 2 2 2直接变为4
 							tx = 0;
 							ty = 0;
-							y--;
+							y--;// 合并后格子变为0，需重新检查
 							break;
 						} else if (cardStatus[x][i].getValue() != 0) {
 							v = cardStatus[x][i].getValue();
 							tx = x;
 							ty = y;
-							cardStatus[x][y].setValue(v);
-							cardStatus[x][i].setValue(0);
-
-							isChange = true;// 格子移动
-							// System.out.println(x+" "+y);
+							move(x, y, x, i);
 							break;
 						}
-					}
+					} // end of for
 
 				} else if (cardStatus[x][y].getValue() == v) {
 					// 合并操作
-					cardStatus[tx][ty].setValue(2 * v);
-					// System.out.println(x+" " +y+ " "+2*v);
-					cardStatus[x][y].setValue(0);
-					isChange = true;// 格子合并
+					merge(tx, ty, x, y);
 					v = -1;
 					tx = 0;
 					ty = 0;
@@ -208,41 +364,103 @@ public class Game {
 					v = cardStatus[x][y].getValue();
 					tx = x;
 					ty = y;
-
 				} // end of if
 			}
-		}
+		} // 移动卡片结束
 
-		/*
-		 * test for (y = 0; y < cardStatus[0].length; y++) { for (x = 0; x <
-		 * cardStatus.length; x++) { cardStatus[x][y].getValue();
-		 * System.out.println(x + " " + y); } } //test end
-		 */
-		// 重新计算空卡片位置
-		calcEmptyCard();
-		if (emptyCard.isEmpty()) {
-			// 游戏结束
-			// gameover();
-			return false;
-		}
-		if (isChange) {// 此次移动有效
-			addNewCard();
-		} else {// 移动无效
-			undoStack.pop();// 删除保存的状态
-		}
 		return true;
 	}
 
+	/**
+	 * 将game向右滑动
+	 * 
+	 * @return 移动是否成功
+	 * @version 创建时间：2016年6月30日21:09:43
+	 * @return
+	 */
+	private boolean slideRight() {
+		// 移动卡片 slideRight
+		for (int x = 0; x < cardStatus.length; x++) {
+			int tx = 0;// 临时x
+			int ty = 0;// 临时y
+			int v = -1;// 保留前一个方格的值
+			for (int y = cardStatus[x].length - 1; y >= 0; y--) {
+				// 值为0
+				if (cardStatus[x][y].getValue() == 0) {
+					// 尝试找到非0值填充，或进行合并
+					for (int i = y - 1; i >= 0; i--) {
+						// 找到值和空格前格子值相同，进行合并
+						if (cardStatus[x][i].getValue() == v) {
+							// 合并操作
+							merge(tx, ty, x, i);
+							// 清空临时变量
+							v = -1;// 不允许重复合并，不允许如2 2 2 2直接变为4
+							tx = 0;
+							ty = 0;
+							y++;// 合并后格子变为0，需重新检查
+							break;
+						} else if (cardStatus[x][i].getValue() != 0) {
+							v = cardStatus[x][i].getValue();
+							tx = x;
+							ty = y;
+							move(x, y, x, i);
+							break;
+						}
+					} // end of for
+
+				} else if (cardStatus[x][y].getValue() == v) {
+					// 合并操作
+					merge(tx, ty, x, y);
+					v = -1;
+					tx = 0;
+					ty = 0;
+					y++;
+				} else {
+					v = cardStatus[x][y].getValue();
+					tx = x;
+					ty = y;
+				} // end of if
+			}
+		} // 移动卡片结束
+
+		return true;
+	}
+
+	// TODO 动画接口
 	// 合并x2y2位置卡片到x1y1卡片，值为x1y1的值*2
 	private boolean merge(int x1, int y1, int x2, int y2) {
-		// TODO 合并x2y2到x1y1，值为x1y1的值*2
+		// TODO 计算分数
+		cardStatus[x1][y1].setValue(2 * cardStatus[x1][y1].getValue());
+		cardStatus[x2][y2].setValue(0);
+		nowScore+=cardStatus[x1][y1].getValue();
+		isChange = true;// 布局发生改变
 		return true;
 	}
 
-	// 将x1y1卡片移动到x2y2
+	// 将x2y2卡片移动到x1y1
 	private boolean move(int x1, int y1, int x2, int y2) {
-		// TODO 将x1y1移动到x2y2
+		cardStatus[x1][y1].setValue(cardStatus[x2][y2].getValue());
+		cardStatus[x2][y2].setValue(0);
+		isChange = true;// 布局发生改变
 		return true;
+	}
+
+	private void gameover() {
+		if (nowScore>maxScore) {
+			maxScore=nowScore;
+			saveScore();
+		}
+		//controller.gameover();
+		
+		return;
+	}
+
+	/*
+	 * 保存成绩
+	 */
+	private void saveScore() {
+		// TODO Auto-generated method stub
+		
 	}
 
 	/**
@@ -272,16 +490,38 @@ public class Game {
 	}
 
 	/**
+	 * 保存传递的状态
+	 * 
+	 * @version 创建时间：2016年6月29日23:22:02
+	 * @param temp
+	 */
+	public void saveStatus(Card[][] temp) {
+		// 判断堆栈是否满
+		if (undoStack.size() < maxUndo) {
+			undoStack.push(temp);
+		} else if (maxUndo > 0) {// 移除最远状态，加入新状态
+			undoStack.remove(undoStack.firstElement());
+			undoStack.push(temp);
+		}
+
+		return;
+	}
+
+	/**
 	 * 撤销函数 回退到上一步状态
 	 *
 	 * @return 撤销成功返回true
 	 * @version 2016年6月27日22:44:44
 	 */
+	
 	public boolean undoStatus() {
 		if (undoStack == null || undoStack.isEmpty()) {
 			return false;
 		}
 		cardStatus = undoStack.pop();
+		step--;
+		//undo之后当前分数不会恢复为旧分数
+		nowScore/=2;// undo之后分数减半，以暂时修补无法回复的缺陷
 		calcEmptyCard();// 重新计算空卡片
 		return true;
 	}
@@ -292,6 +532,25 @@ public class Game {
 	 */
 	public int undoCount() {
 		return undoStack.size();
+	}
+	
+	/**
+	 * @return the maxScore
+	 * @version 创建时间：2016年6月30日21:18:11
+	 */
+	public int getMaxScore() {
+		return maxScore;
+	}
+
+	public int getNowScore() {
+		return nowScore;
+	}
+
+	/**
+	 * @return the step
+	 */
+	public int getStep() {
+		return step;
 	}
 
 }
--- src/pers/jmu/test/GameTest.java
+++ src/pers/jmu/test/GameTest.java
@@ -1,5 +1,6 @@
 package pers.jmu.test;
 
+import java.util.Scanner;
 
 import pers.jmu.model.Card;
 import pers.jmu.model.Game;
@@ -13,19 +14,54 @@ public class GameTest {
 
 	public static void main(String[] args) {
 		Game game = new Game();
-		Card[][] gameStatus = game.getCardStatus();
-		//displayStatus(gameStatus);
-		game.saveStatus();
-		//game.addNewCard();
-		game.setCardValue(0, 1, 8);
-		game.setCardValue(0, 3, 8);
-		displayStatus(gameStatus);
-		//game.undoStatus();
-		
-		System.out.println("当前可回退次数:"+game.undoCount());
-		game.slideLeft();
+		game.setCardValue(3, 0, 8);
+		game.setCardValue(3, 1, 8);
 		displayStatus(game.getCardStatus());
-		return;
+
+		while (true) {
+			Scanner sc = new Scanner(System.in);
+			String s;
+			s = sc.next();
+			char c;
+			c = s.charAt(0);
+			switch (c) {
+			case 'w':
+				game.slideTo(Game.SLIDE_UP);
+				break;
+			case 's':
+				game.slideTo(Game.SLIDE_DOWN);
+				break;
+			case 'a':
+				game.slideTo(Game.SLIDE_LEFT);
+				break;
+			case 'd':
+				game.slideTo(Game.SLIDE_RIGHT);
+				break;
+			case 'r':
+				game.restart();
+				System.out.println("重新开始");
+				break;
+			case 'm':
+				System.out.println("最高分：" + game.getMaxScore());
+			case 'e':
+				System.out.println("当前分数:" + game.getNowScore());
+				break;
+			case 'u':
+				game.undoStatus();
+				System.out.println("当前可回退次数:" + game.undoCount());
+				break;
+			case 'q':
+				sc.close();
+				System.out.println("离开游戏");
+				System.exit(0);
+				break;
+			default:
+				System.out.println(c);
+				break;
+			}
+			displayStatus(game.getCardStatus());
+		}
+
 	}
 
 	/*
